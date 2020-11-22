1b5410bc44336698569d5a47f77ca2366c7ad525
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-11-22 15:58:38 +0800

    format

--- pg_drive/examples/enjoy_maps.py
+++ pg_drive/examples/enjoy_maps.py
@@ -5,13 +5,18 @@ from pg_drive.utils import setup_logger
 setup_logger(debug=True)
 
 if __name__ == "__main__":
-    env = GeneralizationRacing(dict(
-        map_config={"type": BigGenerateMethod.BLOCK_NUM, "config": 7, },
-        camera_height=1000.0,
-        use_render=True,
-        environment_num=100,
-        traffic_density=0.0,
-    ))
+    env = GeneralizationRacing(
+        dict(
+            map_config={
+                "type": BigGenerateMethod.BLOCK_NUM,
+                "config": 7,
+            },
+            camera_height=1000.0,
+            use_render=True,
+            environment_num=100,
+            traffic_density=0.0,
+        )
+    )
     env.reset()
     for i in range(1, 100000):
         o, r, d, info = env.step([0.1, 0])
