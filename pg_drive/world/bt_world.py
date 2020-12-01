b7401dbae10484e4aae4b960a3b1cfcf37875832
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-01 17:56:26 +0800

    format

--- pg_drive/world/bt_world.py
+++ pg_drive/world/bt_world.py
@@ -118,7 +118,8 @@ class BtWorld(ShowBase.ShowBase):
             lens.setAspectRatio(1.2)
 
             self.sky_box = SkyBox(
-                self.bt_config["headless_rgb"] or sys.platform == "darwin")  # openGl shader didn't work for mac...
+                self.bt_config["headless_rgb"] or sys.platform == "darwin"
+            )  # openGl shader didn't work for mac...
             self.sky_box.add_to_render_module(self.render)
 
             self.light = Light(self.bt_config)
