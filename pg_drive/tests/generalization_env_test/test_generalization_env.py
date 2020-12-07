85a9dd7cb3f5d4565ea19e3c86b1ec5119839d2f
lqy0057 <785878978@qq.com>
2020-12-07 18:18:18 +0800

    fix over

--- pg_drive/tests/generalization_env_test/test_generalization_env.py
+++ pg_drive/tests/generalization_env_test/test_generalization_env.py
@@ -11,11 +11,12 @@ class ResetEnv(GeneralizationRacing):
         super(ResetEnv, self).__init__(
             {
                 "environment_num": 4,
-                "traffic_density": 0.1,
+                "traffic_density": 0.5,
                 "start_seed": 3,
                 "pg_world_config": {
                     "debug": False,
                 },
+                "traffic_mode":TrafficMode.Reborn,
                 "image_buffer_name": "mini_map",
                 "manual_control": True,
                 "use_render": True,
@@ -24,8 +25,8 @@ class ResetEnv(GeneralizationRacing):
                 "decision_repeat": 5,
                 "rgb_clip": True,
                 "map_config": {
-                    Map.GENERATE_METHOD: MapGenerateMethod.BIG_BLOCK_NUM,
-                    Map.GENERATE_PARA: 12,
+                    Map.GENERATE_METHOD: MapGenerateMethod.BIG_BLOCK_SEQUENCE,
+                    Map.GENERATE_PARA: "TTTTTTT",
                     Map.LANE_WIDTH: 3.5,
                     Map.LANE_NUM: 3,
                 }
