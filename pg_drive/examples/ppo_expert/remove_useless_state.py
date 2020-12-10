3649299901445837611d8ecedca3484080b1eb9f
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-10 16:58:48 +0800

    introduce numpy version of experts

--- pg_drive/examples/ppo_expert/remove_useless_state.py
+++ pg_drive/examples/ppo_expert/remove_useless_state.py
@@ -1,19 +1,22 @@
 """
 This script is used to remove the optimizer state in the checkpoint. So that we can compress 2/3 of the checkpoint size.
-Remember to rename checkpoint-XXX.tune_metadata to checkpoint-NEWNAME.tune_metadata
+This script is put here for reference only. In formal release, the original checkpoint file will be removed so
+this script will become not runnable.
 """
-
+import os.path as osp
 import pickle
 
-if __name__ == '__main__':
-    with open("checkpoint-630", "rb") as f:
-        data = f.read()
+import numpy as np
 
+ckpt_path = osp.join(osp.dirname(__file__), "checkpoint-compressed")
+if __name__ == '__main__':
+    with open(ckpt_path, "rb") as f:
+        data = f.read()
     unpickled = pickle.loads(data)
     worker = pickle.loads(unpickled.pop("worker"))
-    worker["state"]["default_policy"].pop("_optimizer_variables")
+    if "_optimizer_variables" in worker["state"]["default_policy"]:
+        worker["state"]["default_policy"].pop("_optimizer_variables")
     pickled_worker = pickle.dumps(worker)
-
-    to_pickled = {"worker": pickled_worker, **unpickled}
-    with open("checkpoint-compressed", "wb") as f:
-        pickle.dump(to_pickled, f)
+    weights = worker["state"]["default_policy"]
+    weights = {k: v for k, v in weights.items() if "value" not in k}
+    np.savez_compressed("expert_weights.npz", **weights)
