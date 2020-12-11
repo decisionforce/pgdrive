e24e2fd154d79e54172ea16d358f914338b0a1c9
lqy0057 <785878978@qq.com>
2020-12-11 15:25:53 +0800

    install test script

---
+++ pg_drive/tests/install_test/test_get_image.py
@@ -0,0 +1,32 @@
+import time
+
+from pg_drive.envs.generalization_racing import GeneralizationRacing
+from pg_drive.scene_creator.algorithm.BIG import BigGenerateMethod
+from pg_drive.scene_creator.map import Map
+
+if __name__ == "__main__":
+    headless = False
+    env = GeneralizationRacing(
+        dict(
+            use_render=False,
+            map_config={
+                Map.GENERATE_METHOD: BigGenerateMethod.BLOCK_NUM,
+                Map.GENERATE_PARA: 7
+            },
+            traffic_density=0.5,
+            use_image=True,
+            pg_world_config=dict(headless_image=headless)
+        )
+    )
+    start = time.time()
+    env.reset()
+    print("Render cost time: ", time.time() - start)
+    for i in range(3):
+        o, r, d, info = env.step([0, 1])
+        from panda3d.core import PNMImage
+
+        img = PNMImage()
+        env.pg_world.win.getScreenshot(img)
+        img.write("{}_{}.png".format("headless" if headless else "local", i))
+    env.close()
+    print("Offscreen render successfully run! Images is saved, Please check thm.")
--- pg_drive/tests/install_test/test_get_image_headless.py
+++ pg_drive/tests/install_test/test_get_image_headless.py
@@ -5,7 +5,7 @@ from pg_drive.scene_creator.algorithm.BIG import BigGenerateMethod
 from pg_drive.scene_creator.map import Map
 
 if __name__ == "__main__":
-    headless = False
+    headless = True
     env = GeneralizationRacing(
         dict(
             use_render=False,
@@ -14,20 +14,16 @@ if __name__ == "__main__":
                 Map.GENERATE_PARA: 7
             },
             traffic_density=0.5,
-            manual_control=True,
-            traffic_mode=0,
             use_image=True,
             pg_world_config=dict(headless_image=headless)
         )
     )
-
     start = time.time()
     env.reset()
     print("Render cost time: ", time.time() - start)
-    for i in range(30):
+    for i in range(3):
         o, r, d, info = env.step([0, 1])
         from panda3d.core import PNMImage
-
         img = PNMImage()
         env.pg_world.win.getScreenshot(img)
         img.write("{}_{}.png".format("headless" if headless else "local", i))
