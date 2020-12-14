0e865ebd0e25b70778b57a23c5179698c4d1aaed
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-15 03:32:41 +0800

    fix a bug (it seems the lazy init is not working)

--- pgdrive/envs/change_friction_env.py
+++ pgdrive/envs/change_friction_env.py
@@ -14,24 +14,20 @@ class ChangeFrictionEnv(PGDriveEnv):
 
     def __init__(self, config=None):
         super(ChangeFrictionEnv, self).__init__(config)
-        self.parameter_list = None
-
-    def get_parameter_list(self):
-        ret = dict()
+        self.parameter_list = dict()
         for k in self.maps:
-            ret[k] = dict(wheel_friction=np.random.uniform(self.config["friction_min"], self.config["friction_max"]))
-        return ret
+            self.parameter_list[k] = dict(
+                wheel_friction=np.random.uniform(self.config["friction_min"], self.config["friction_max"])
+            )
 
     def reset(self):
-        if self.parameter_list is None:
-            # Sometimes the seed is given later than initialization.
-            self.parameter_list = self.get_parameter_list()
         index = np.random.choice(list(self.parameter_list.keys()))
         parameter = self.parameter_list[index]
         if self.vehicle is None or (not self.config["change_friction"]):
             pass
         else:
             self.vehicle.vehicle_config["wheel_friction"] = parameter["wheel_friction"]
+            print("The friction is changed to: ", parameter["wheel_friction"])
         ret = super(ChangeFrictionEnv, self).reset()
         return ret
 
@@ -41,7 +37,7 @@ if __name__ == '__main__':
     env.seed(100000)
     obs = env.reset()
     for s in range(10000):
-        action = np.array([1.0, 1.0])
+        action = np.array([0.0, 1.0])
         o, r, d, i = env.step(action)
         if d:
             env.reset()
