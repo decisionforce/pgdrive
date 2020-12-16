f9936dc5d4f9e8a0a5f86d3ed917e2b6c9cdc69e
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-16 18:27:34 +0800

    format

--- pgdrive/tests/highway_render_test/profile_map_generation.py
+++ pgdrive/tests/highway_render_test/profile_map_generation.py
@@ -4,14 +4,16 @@ from pgdrive import PGDriveEnv
 from pgdrive.scene_creator.map import Map, MapGenerateMethod
 
 env = PGDriveEnv(
-    dict(environment_num=1,
-         map_config={
-             Map.GENERATE_METHOD: MapGenerateMethod.BIG_BLOCK_SEQUENCE,
-             Map.GENERATE_PARA: "OCrRCTXRCCCCrOr",
-             Map.LANE_WIDTH: 3.5,
-             Map.LANE_NUM: 3,
-         }
-         ))
+    dict(
+        environment_num=1,
+        map_config={
+            Map.GENERATE_METHOD: MapGenerateMethod.BIG_BLOCK_SEQUENCE,
+            Map.GENERATE_PARA: "OCrRCTXRCCCCrOr",
+            Map.LANE_WIDTH: 3.5,
+            Map.LANE_NUM: 3,
+        }
+    )
+)
 
 for i in range(100):
     env.reset()
