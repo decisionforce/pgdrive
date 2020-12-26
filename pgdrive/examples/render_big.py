0a7dad96df7d8bc66753cfd273f8aab461332db0
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-26 15:03:13 +0800

    fix enjoy exper

--- pgdrive/examples/render_big.py
+++ pgdrive/examples/render_big.py
@@ -1,7 +1,8 @@
-from pgdrive.tests.block_test.test_big import vis_big
-from pgdrive.world.pg_world import root_path
 import sys
+
+from pgdrive.tests.block_test.test_big import vis_big
 from pgdrive.utils.asset_loader import AssetLoader
+from pgdrive.world.pg_world import root_path
 
 if __name__ == "__main__":
     path = AssetLoader.windows_style2unix_style(root_path) if sys.platform == "win32" else root_path
