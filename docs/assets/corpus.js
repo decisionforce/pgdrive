74d3c5c9be91959b2fc4f09a9a915cc2f12afa03
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-27 18:02:55 +0800

    update some words

--- docs/assets/corpus.js
+++ docs/assets/corpus.js
@@ -1,5 +1,5 @@
 var title_corpus = [
-    ['PGDrive: An Open-ended Driving Simulator <br>with Infinite Scenes', 'PG驾驶：一个拥有无限场景的开放驾驶平台'],
+    ['PGDrive: An Open-ended Driving Simulator <br>with Infinite Scenes', 'PGDrive：一个拥有无限场景的开放式驾驶平台'],
     ['Overview', '总览'],
     ['Results', '结果'],
 ]
@@ -22,11 +22,11 @@ var text_corpus = [
     PGDrive can generate infinite driving scenes through procedural generalization, which can benifit the research\
     on the generalization of learning system.\
     Please visit <a href="https://github.com/decisionforce/pgdrive">github.com/decisionforce/pgdrive</a> to enjoy\
-    PGDrive!', '为了更好地评估和改善基于强化学习的自动驾驶系统的范化性能， 我们提出了一个开放、极易配置的模拟器：PG开车！PG开车可以通过过程生成生成无限多的地图\
-    ，从而造福面向学习系统范化性能的研究。欢迎访问 <a href="https://github.com/decisionforce/pgdrive">github.com/decisionforce/pgdrive</a> 来亲身感受！'],
+    PGDrive!', '为了更好地评估和改善基于强化学习的自动驾驶系统的泛化性能，我们提出了一个开放式、极易配置的模拟器：PGDrive！PGDrive可以通过过程生成技术生成无限多的地图\
+    ，从而助力学习系统泛化性能的研究。欢迎访问 <a href="https://github.com/decisionforce/pgdrive">github.com/decisionforce/pgdrive</a> 来亲身感受！'],
     ['We generate maps with these elementary road blocks:', '我们用了以下基础路块来生成地图：'],
-    ['Here are various generated maps:', '以下是生成的地图'],
-    ['Agents trained with more maps show better test performance, showing that PGDrive endows agent with generality:', '在训练中见识过更多地图的智能体展现出了更优越的测试性能。这无疑说明了我们的PG开车赋予了智能体更强大的泛化能力！'],
+    ['Here are various generated maps:', '以下是生成的地图：'],
+    ['Agents trained with more maps show better test performance, showing that PGDrive endows agent with generality:', '在训练中见识过更多地图的智能体展现出了更优越的测试性能。这说明了我们的PGDrive赋予了智能体更强大的泛化能力！'],
     ['The training code for the paper can be founded<a href="https://github.com/decisionforce/pgdrive-generalization-paper">here</a>.', '我们的训练代码在<a href="https://github.com/decisionforce/pgdrive-generalization-paper">这里</a>。'],
     ['View our trained agents in the following video:', '请在如下视频中欣赏我们训练的智能体：'],
     ['Citation', '引用']
@@ -36,7 +36,7 @@ var bar_corpus = [
     ['Github Repo', 'GitHub仓库'],
     ['Documentation', '文档'],
     ['Paper', '论文'],
-    ['切换中文', 'to English']
+    ['切换中文', 'To English']
 ]
 var lang_flag = 1;
 
