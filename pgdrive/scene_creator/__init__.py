3254e0bd39107eb13dd7f7b7e421b71f4f7aa921
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-29 10:34:37 +0800

    format

--- pgdrive/scene_creator/__init__.py
+++ pgdrive/scene_creator/__init__.py
@@ -1,2 +1,1 @@
 from pgdrive.scene_creator.basic_utils import Decoration, Goal, get_road_bound_box, check_lane_on_road
-# from pgdrive.scene_creator.create_utils import CreateRoadFrom, CreateAdverseRoad, ExtendStraightLane, sharpbend
