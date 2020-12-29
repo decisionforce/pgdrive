c1374806c575b23b9c17644c85b09e6c0d4ca11e
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-29 14:13:18 +0800

    format

--- pgdrive/tests/install_test/test_headless_install.py
+++ pgdrive/tests/install_test/test_headless_install.py
@@ -2,7 +2,6 @@ from panda3d.core import loadPrcFileData
 
 from pgdrive.tests.get_image import test_install
 
-
 if __name__ == "__main__":
     loadPrcFileData("", "notify-level-task fatal")
     test_install(True)
