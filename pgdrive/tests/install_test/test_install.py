c22b5687cc6856a1f8732acfba2f5c1659d1ae22
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-29 14:10:31 +0800

    add CI

--- pgdrive/tests/install_test/test_install.py
+++ pgdrive/tests/install_test/test_install.py
@@ -1,7 +1,7 @@
-from pgdrive.tests.install_test.get_image import test_install
 from panda3d.core import loadPrcFileData
 
-loadPrcFileData("", "notify-level-task fatal")
+from pgdrive.tests.install_test.get_image import test_install
 
 if __name__ == "__main__":
+    loadPrcFileData("", "notify-level-task fatal")
     test_install(False)
