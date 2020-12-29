2d26ce2ecac3bb5ab08e0f1525a82fc0005ef645
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-29 21:50:07 +0800

    update map image retrieve

--- pgdrive/scene_creator/map.py
+++ pgdrive/scene_creator/map.py
@@ -2,16 +2,17 @@ import copy
 import json
 import logging
 import os
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Iterable
 
+import cv2
 import numpy as np
 from panda3d.bullet import BulletWorld
 from panda3d.core import NodePath
+
 from pgdrive.pg_config import PgConfig
 from pgdrive.pg_config.pg_blocks import PgBlock
 from pgdrive.scene_creator import get_road_bound_box
 from pgdrive.scene_creator.algorithm.BIG import BIG, BigGenerateMethod
-from pgdrive.scene_creator.basic_utils import Decoration
 from pgdrive.scene_creator.blocks.block import Block
 from pgdrive.scene_creator.blocks.first_block import FirstBlock
 from pgdrive.scene_creator.road.road_network import RoadNetwork
@@ -203,9 +204,7 @@ class Map:
             ret = self.read_map(map_config)
         return ret
 
-    def draw_maximum_surface(self, simple_draw: bool):
-        from pgdrive.world.highway_render.highway_render import LaneGraphics
-        from pgdrive.world.highway_render.world_surface import WorldSurface
+    def draw_maximum_surface(self) -> pygame.Surface:
         surface = WorldSurface(self.film_size, 0, pygame.Surface(self.film_size))
         b_box = self.get_map_bound_box(self.road_network)
         x_len = b_box[1] - b_box[0]
@@ -217,18 +216,18 @@ class Map:
         centering_pos = ((b_box[0] + b_box[1]) / 2, (b_box[2] + b_box[3]) / 2)
         surface.move_display_window_to(centering_pos)
         for _from in self.road_network.graph.keys():
-            decoration = True if _from == Decoration.start else False
+            # decoration = True if _from == Decoration.start else False
             for _to in self.road_network.graph[_from].keys():
                 for l in self.road_network.graph[_from][_to]:
-                    if simple_draw:
-                        LaneGraphics.simple_draw(l, surface)
-                    else:
-                        two_side = True if l is self.road_network.graph[_from][_to][-1] or decoration else False
-                        LaneGraphics.display(l, surface, two_side)
+                    # if simple_draw:
+                    LaneGraphics.simple_draw(l, surface)
+                    # else:
+                    # two_side = True if l is self.road_network.graph[_from][_to][-1] or decoration else False
+                    # LaneGraphics.display(l, surface, two_side)
         return surface
 
-    def draw_map_image_on_surface(self, dest_resolution=(512, 512), simple_draw=True) -> pygame.Surface:
-        surface = self.draw_maximum_surface(simple_draw=simple_draw)
+    def draw_map_image_on_surface(self, dest_resolution=(512, 512)) -> pygame.Surface:
+        surface = self.draw_maximum_surface()
         dest_surface = pygame.Surface(dest_resolution)
         pygame.transform.scale(surface, dest_resolution, dest_surface)
         return dest_surface
@@ -250,43 +249,16 @@ class Map:
                 res_y_min = min(res_y_min, y_min)
         return res_x_min, res_x_max, res_y_min, res_y_max
 
-    def get_map_image_array(
-        self,
-        resolution=(512, 512),
-        fill_hole=False,
-        only_black_white=True,
-        return_surface=False,
-        simple_draw=True
-    ) -> Optional[Union[np.ndarray, pygame.Surface]]:
-        # surface = self.draw_map_image_on_surface(resolution, simple_draw=simple_draw)
-        # TODO change the surface to dest resolution from (8192, 8192)
-        surface = self.draw_maximum_surface(simple_draw)
-
-        # Not finished! Just put it WIP
-        import cv2
+    def get_map_image_array(self, resolution: Iterable = (512, 512)) -> Optional[Union[np.ndarray, pygame.Surface]]:
+        surface = self.draw_maximum_surface()
         ret = cv2.resize(pygame.surfarray.pixels_red(surface), resolution, interpolation=cv2.INTER_LINEAR)
         return ret
 
-        if fill_hole:
-            surface = self.fill_hole(surface)
-        if only_black_white:
-            return np.clip(pygame.surfarray.pixels_red(surface), 0.0, 1.0)
-        if return_surface:
-            return surface
-        """
-        change the return value
-        """
-        return pygame.surfarray.array3d(surface)
-
-    def save_map_image(self, resolution=(2048, 2048), fill_hole=False, only_black_white=False, simple_draw=True):
-        surface = self.get_map_image_array(
-            resolution=resolution,
-            fill_hole=fill_hole,
-            only_black_white=only_black_white,
-            return_surface=True,
-            simple_draw=simple_draw
-        )
-        pygame.image.save(surface, "map_{}.png".format(self.random_seed))
+    def save_map_image(self, resolution: Iterable = (2048, 2048)):
+        surface = self.get_map_image_array(resolution=resolution)
+        import matplotlib.pyplot as plt
+        plt.imshow(surface, cmap="Greys")
+        plt.savefig("map_{}.png".format(self.random_seed))
 
     @staticmethod
     def fill_hole(surface: pygame.Surface, threshold=3, kernal=(3, 3)):
@@ -330,30 +302,30 @@ class Map:
                 if 0 < x + x_1 < x_size and 0 < y + y_1 < y_size:
                     count_a[x + x_1][y + y_1] += 1
 
-    def draw_map_with_navi_lines(self, vehicle, dest_resolution=(512, 512), save=False, navi_line_color=(255, 0, 0)):
-        checkpoints = vehicle.routing_localization.checkpoints
-        map_surface = self.draw_map_image_on_surface(dest_resolution=dest_resolution, simple_draw=False)
-        surface = WorldSurface(self.film_size, 0, pygame.Surface(self.film_size))
-        b_box = self.get_map_bound_box(self.road_network)
-        x_len = b_box[1] - b_box[0]
-        y_len = b_box[3] - b_box[2]
-        max_len = max(x_len, y_len)
-        # scaling and center can be easily found by bounding box
-        scaling = self.film_size[1] / max_len - 0.1
-        surface.scaling = scaling
-        centering_pos = ((b_box[0] + b_box[1]) / 2, (b_box[2] + b_box[3]) / 2)
-        surface.move_display_window_to(centering_pos)
-        for i, c in enumerate(checkpoints[:-1]):
-            lanes = self.road_network.graph[c][checkpoints[i + 1]]
-            for lane in lanes:
-                LaneGraphics.simple_draw(lane, surface, color=navi_line_color)
-        dest_surface = pygame.Surface(dest_resolution)
-        pygame.transform.scale(surface, dest_resolution, dest_surface)
-        dest_surface.set_alpha(100)
-        map_surface.blit(dest_surface, (0, 0))
-        if save:
-            pygame.image.save(map_surface, "map_{}.png".format(self.random_seed))
-        return map_surface
+    # def draw_map_with_navi_lines(self, vehicle, dest_resolution=(512, 512), save=False, navi_line_color=(255, 0, 0)):
+    #     checkpoints = vehicle.routing_localization.checkpoints
+    #     map_surface = self.draw_map_image_on_surface(dest_resolution=dest_resolution, simple_draw=False)
+    #     surface = WorldSurface(self.film_size, 0, pygame.Surface(self.film_size))
+    #     b_box = self.get_map_bound_box(self.road_network)
+    #     x_len = b_box[1] - b_box[0]
+    #     y_len = b_box[3] - b_box[2]
+    #     max_len = max(x_len, y_len)
+    #     # scaling and center can be easily found by bounding box
+    #     scaling = self.film_size[1] / max_len - 0.1
+    #     surface.scaling = scaling
+    #     centering_pos = ((b_box[0] + b_box[1]) / 2, (b_box[2] + b_box[3]) / 2)
+    #     surface.move_display_window_to(centering_pos)
+    #     for i, c in enumerate(checkpoints[:-1]):
+    #         lanes = self.road_network.graph[c][checkpoints[i + 1]]
+    #         for lane in lanes:
+    #             LaneGraphics.simple_draw(lane, surface, color=navi_line_color)
+    #     dest_surface = pygame.Surface(dest_resolution)
+    #     pygame.transform.scale(surface, dest_resolution, dest_surface)
+    #     dest_surface.set_alpha(100)
+    #     map_surface.blit(dest_surface, (0, 0))
+    #     if save:
+    #         pygame.image.save(map_surface, "map_{}.png".format(self.random_seed))
+    #     return map_surface
 
     def __del__(self):
         describe = self.random_seed if self.random_seed is not None else "custom"
