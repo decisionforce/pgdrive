8c49dc197c05dce84c934630d933087cc8832853
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2020-12-30 20:03:39 +0800

    update test scrtip

--- pgdrive/tests/test_action_repeat_env.py
+++ pgdrive/tests/test_action_repeat_env.py
@@ -1,4 +1,5 @@
 import pytest
+
 from pgdrive import ActionRepeat
 
 
@@ -6,18 +7,25 @@ def _test_action_repeat(config):
     env = ActionRepeat(config)
     env.reset()
     for i in range(100):
-        _, _, d, _ = env.step(env.action_space.sample())
+        _, _, d, info = env.step(env.action_space.sample())
+
+        for k in ["simulation_time", "real_return", "action_repeat", "primitive_steps_count", "max_step", "render",
+                  "trajectory"]:
+            assert k in info
+
+        assert len(info["trajectory"]) == info["action_repeat"]
+        for t in info["trajectory"]:
+            for k in ["reward", "discounted_reward", "obs", "action", "count"]:
+                assert k in t
+
         if d:
             env.reset()
     env.close()
 
 
-def test_action_repeat_max_ar():
+def test_action_repeat_env():
     _test_action_repeat(dict(max_action_repeat=5))
     _test_action_repeat(dict(max_action_repeat=1))
-
-
-def test_action_repeat_fixed_ar():
     _test_action_repeat(dict(fixed_action_repeat=5))
     _test_action_repeat(dict(fixed_action_repeat=1))
 
