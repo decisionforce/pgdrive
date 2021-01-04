976cfab71bbe4358bc8d7fc38fb63ff656dcf288
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-01-05 00:08:43 +0800

    introduce a config script

--- pgdrive/tests/test_top_down_rendering/test_get_map_image.py
+++ pgdrive/tests/test_top_down_rendering/test_get_map_image.py
@@ -11,7 +11,7 @@ def test_save_map_image():
     setup_logger(debug=True)
     env = PGDriveEnv(dict(environment_num=20, start_seed=0, map=10))
     try:
-        for i in range(20):
+        for i in range(5):
             env.reset()
             surface = env.get_map(resolution=(128, 128))
             plt.imshow(surface, cmap="Greys")
