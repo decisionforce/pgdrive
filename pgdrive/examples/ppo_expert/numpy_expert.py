2e14557dbbf622b010af8e057f647624135dba46
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-01-08 23:33:07 +0800

    add a test expert script

--- pgdrive/examples/ppo_expert/numpy_expert.py
+++ pgdrive/examples/ppo_expert/numpy_expert.py
@@ -23,7 +23,7 @@ ckpt_path = osp.join(osp.dirname(__file__), "expert_weights.npz")
 _expert_weights = None
 
 
-def expert(obs):
+def expert(obs, deterministic=False):
     global _expert_weights
     if _expert_weights is None:
         _expert_weights = np.load(ckpt_path)
@@ -36,6 +36,8 @@ def expert(obs):
     x = np.matmul(x, weights["default_policy/fc_out/kernel"]) + weights["default_policy/fc_out/bias"]
     x = x.reshape(-1)
     mean, log_std = np.split(x, 2)
+    if deterministic:
+        return mean
     std = np.exp(log_std)
     action = np.random.normal(mean, std)
     ret = action
@@ -46,5 +48,5 @@ def expert(obs):
 if __name__ == '__main__':
     for i in range(100):
         print("Weights? ", type(_expert_weights))
-        ret = expert(np.clip(np.random.normal(0.5, 1, size=(275, )), 0.0, 1.0))
+        ret = expert(np.clip(np.random.normal(0.5, 1, size=(275,)), 0.0, 1.0))
         print("Return: ", ret)
