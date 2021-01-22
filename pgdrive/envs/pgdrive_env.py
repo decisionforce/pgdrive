a4b63e3bfcc86e5c8fd4225d1780ceb33768b817
lqy0057 <785878978@qq.com>
2021-01-22 13:18:58 +0800

    format

--- pgdrive/envs/pgdrive_env.py
+++ pgdrive/envs/pgdrive_env.py
@@ -98,7 +98,7 @@ class PGDriveEnv(gym.Env):
         self.observation = LidarStateObservation(vehicle_config) if not self.config["use_image"] \
             else ImageStateObservation(vehicle_config, self.config["image_source"], self.config["rgb_clip"])
         self.observation_space = self.observation.observation_space
-        self.action_space = gym.spaces.Box(-1.0, 1.0, shape=(2,), dtype=np.float32)
+        self.action_space = gym.spaces.Box(-1.0, 1.0, shape=(2, ), dtype=np.float32)
 
         self.start_seed = self.config["start_seed"]
         self.env_num = self.config["environment_num"]
@@ -154,9 +154,9 @@ class PGDriveEnv(gym.Env):
         self.pg_world.accept("t", self.toggle_expert_take_over)
 
         # init traffic manager
-        self.scene_manager = SceneManager(self.pg_world,
-                                          self.config["traffic_mode"], self.config["random_traffic"],
-                                          self.config["record_episode"])
+        self.scene_manager = SceneManager(
+            self.pg_world, self.config["traffic_mode"], self.config["random_traffic"], self.config["record_episode"]
+        )
 
         if self.config["manual_control"]:
             if self.config["controller"] == "keyboard":
@@ -286,7 +286,7 @@ class PGDriveEnv(gym.Env):
         steering_penalty = self.config["steering_penalty"] * steering_change * self.vehicle.speed / 20
         reward -= steering_penalty
         # Penalty for frequent acceleration / brake
-        acceleration_penalty = self.config["acceleration_penalty"] * ((action[1]) ** 2)
+        acceleration_penalty = self.config["acceleration_penalty"] * ((action[1])**2)
         reward -= acceleration_penalty
 
         # Penalty for waiting
--- pgdrive/scene_manager/scene_manager.py
+++ pgdrive/scene_manager/scene_manager.py
@@ -15,12 +15,13 @@ Route = List[LaneIndex]
 
 class SceneManager:
     """Manage all traffic vehicles, and all runtime elements (in the future)"""
-
-    def __init__(self,
-                 pg_world: PGWorld,
-                 traffic_mode=TrafficMode.Trigger,
-                 random_traffic: bool = False,
-                 record_episode: bool = False):
+    def __init__(
+        self,
+        pg_world: PGWorld,
+        traffic_mode=TrafficMode.Trigger,
+        random_traffic: bool = False,
+        record_episode: bool = False
+    ):
         """
         :param traffic_mode: reborn/trigger mode
         :param random_traffic: if True, map seed is different with traffic manager seed
