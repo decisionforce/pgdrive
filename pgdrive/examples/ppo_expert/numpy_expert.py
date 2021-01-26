b82f95bc97e1477316ffdd78b843bb08e43d836e
lqy0057 <785878978@qq.com>
2021-01-26 13:57:41 +0800

    vis_value

--- pgdrive/examples/ppo_expert/numpy_expert.py
+++ pgdrive/examples/ppo_expert/numpy_expert.py
@@ -19,7 +19,7 @@ import os.path as osp
 
 import numpy as np
 
-ckpt_path = osp.join(osp.dirname(__file__), "expert_weights.npz")
+ckpt_path = osp.join(osp.dirname(__file__), "baseline.npz")
 _expert_weights = None
 
 
@@ -45,8 +45,34 @@ def expert(obs, deterministic=False):
     return ret
 
 
+def load_weights(path: str):
+    """
+    Load NN weights
+    :param path: weights file path path
+    :return: NN weights object
+    """
+    return np.load(path)
+
+
+def value(obs, weights):
+    """
+    Given weights, return the evaluation to one state/obseration
+    :param obs: observation
+    :param weights: variable weights of NN
+    :return: value
+    """
+    obs = obs.reshape(1, -1)
+    x = np.matmul(obs, weights["default_policy/fc_value_1/kernel"]) + weights["default_policy/fc_value_1/bias"]
+    x = np.tanh(x)
+    x = np.matmul(x, weights["default_policy/fc_value_2/kernel"]) + weights["default_policy/fc_value_2/bias"]
+    x = np.tanh(x)
+    x = np.matmul(x, weights["default_policy/value_out/kernel"]) + weights["default_policy/value_out/bias"]
+    ret = x.reshape(-1)
+    return ret
+
+
 if __name__ == '__main__':
     for i in range(100):
         print("Weights? ", type(_expert_weights))
-        ret = expert(np.clip(np.random.normal(0.5, 1, size=(275, )), 0.0, 1.0))
+        ret = expert(np.clip(np.random.normal(0.5, 1, size=(275,)), 0.0, 1.0))
         print("Return: ", ret)
--- pgdrive/tests/test_pgdrive_env/vis_saver.py
+++ pgdrive/tests/test_pgdrive_env/vis_saver.py
@@ -1,4 +1,5 @@
 from pgdrive.envs.generation_envs.safe_driving_env import SafeDrivingEnv
+from pgdrive.examples.ppo_expert.numpy_expert import value, load_weights
 
 if __name__ == "__main__":
     env = SafeDrivingEnv(
@@ -10,26 +11,43 @@ if __name__ == "__main__":
             # "controller": "joystick",
             "manual_control": True,
             "use_render": True,
-            "out_of_road_constrain":False,
-            "crash_constrain":False,
-            # "save_level":0.3,
+            # "out_of_road_constrain":False,
+            # "crash_constrain":False,
+            # "save_level":0.2,
             # "takeover_penalty":0.5
-            # "use_saver":False,
+            "use_saver":False,
             # "map": "Crrrrrrrrrr"
         }
     )
+    baseline_weights = load_weights("../../examples/ppo_expert/baseline.npz")
+    no_gae_saver_weights = load_weights("../../examples/ppo_expert/no_gae.npz")
 
     o = env.reset()
     max_s = 0
     max_t = 0
+    start = 0
     for i in range(1, 100000000):
+        o_to_evaluate = o
         o, r, d, info = env.step([0, 1])
         max_s = max(max_s, info["raw_action"][0])
         max_t = max(max_t, info["raw_action"][1])
-        text = {"save": env.save_mode, "max_action":(max_s, max_t), "raw_reward":env.step_info["raw_step_reward"],
-                "reward":r, "speed":env.vehicle.speed, "high_speed":info["high_speed"]}
+
+        baseline_value=value(o_to_evaluate, baseline_weights)
+        saver_value=value(o_to_evaluate, no_gae_saver_weights)
+
+        text = {"save": env.save_mode,
+                # "max_action": (max_s, max_t),
+                # "raw_reward": env.step_info["raw_step_reward"],
+                # "reward": r,
+                # "speed": env.vehicle.speed,
+                # "high_speed": info["high_speed"]
+                "baseline_value":baseline_value,
+                "no_gae_value":saver_value,
+                "diff":baseline_value-saver_value
+                }
         env.render(text=text)
         if d:
-            print(info)
+            print("episode_len:", i - start)
             env.reset()
+            start = i
     env.close()
