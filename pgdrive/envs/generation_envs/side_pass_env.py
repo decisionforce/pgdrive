fcc53445dfdb694780d4e8e7a2e4796817db5e4b
lqy0057 <785878978@qq.com>
2021-02-21 15:21:48 +0800

    ok

--- pgdrive/envs/generation_envs/side_pass_env.py
+++ pgdrive/envs/generation_envs/side_pass_env.py
@@ -12,7 +12,7 @@ class SidePassEnv(PGDriveEnv):
                 "debug_physics_world": False,
             },
             "debug": False,
-            "map": "CSC"
+            "map": "CSS"
         }
         if extra_config is not None:
             config.update(extra_config)
@@ -31,12 +31,12 @@ class SidePassEnv(PGDriveEnv):
         self.alert.attach_to_pg_world(self.pg_world.pbr_worldNP, self.pg_world.physics_world)
 
         lane = self.current_map.road_network.graph["1C0_1_"]["2S0_0_"][2]
-        pos = [(-20, lane.width / 3), (-15.6, lane.width / 4), (-12.1, 0),
-               (-8.7, -lane.width / 4), (-4.2, -lane.width / 2), (-0.7, -lane.width * 3 / 4),
-               (4.1, -lane.width), (7.3, -lane.width), (11.5, -lane.width),
-               (15.5, -lane.width), (20.0, -lane.width), (23.2, -lane.width),
-               (29.1, -lane.width), (32.9, -lane.width / 2), (37.0, 0),
-               (40.0, lane.width / 2)]
+        pos = [
+            (-20, lane.width / 3), (-15.6, lane.width / 4), (-12.1, 0), (-8.7, -lane.width / 4),
+            (-4.2, -lane.width / 2), (-0.7, -lane.width * 3 / 4), (4.1, -lane.width), (7.3, -lane.width),
+            (11.5, -lane.width), (15.5, -lane.width), (20.0, -lane.width), (23.2, -lane.width), (29.1, -lane.width),
+            (32.9, -lane.width / 2), (37.0, 0), (40.0, lane.width / 2)
+        ]
 
         for p in pos:
             cone = self.scene_manager.objects_mgr.spawn_one_object("Traffic Cone", lane, *p)
@@ -51,9 +51,9 @@ class SidePassEnv(PGDriveEnv):
         air_wall = IDMVehicle.create_random(self.scene_manager.traffic_mgr, lane, -25, 0, None)
         self.scene_manager.traffic_mgr.vehicles.append(air_wall)
 
-        air_wall = IDMVehicle.create_random(self.scene_manager.traffic_mgr,
-                                            self.current_map.road_network.graph["1C0_1_"]["2S0_0_"][1], -20, 0,
-                                            None)
+        air_wall = IDMVehicle.create_random(
+            self.scene_manager.traffic_mgr, self.current_map.road_network.graph["1C0_1_"]["2S0_0_"][1], -20, 0, None
+        )
         self.scene_manager.traffic_mgr.vehicles.append(air_wall)
 
         return ret
