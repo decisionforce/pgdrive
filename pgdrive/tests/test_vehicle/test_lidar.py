99979c7b1d239bdf8023b556dce988489db651e0
lqy0057 <785878978@qq.com>
2021-02-24 14:27:42 +0800

    format

--- pgdrive/tests/test_vehicle/test_lidar.py
+++ pgdrive/tests/test_vehicle/test_lidar.py
@@ -11,20 +11,22 @@ class TestEnv(PGDriveEnv):
 def test_lidar(render=False):
     setup_logger(debug=True)
     Lidar.enable_show = True
-    env = TestEnv({"use_render": render, "manual_control": render, "environment_num": 1, "traffic_density": 0.3})
-    env.reset()
-    detect_vehicle = False
-    for i in range(1, 100000):
-        o, r, d, info = env.step([0, 1])
+    try:
+        env = TestEnv({"use_render": render, "manual_control": render, "environment_num": 1, "traffic_density": 0.3})
+        env.reset()
+        detect_vehicle = False
+        for i in range(1, 100000):
+            o, r, d, info = env.step([0, 1])
 
-        if len(env.vehicle.lidar.get_surrounding_vehicles()) != 0:
-            detect_vehicle = True
+            if len(env.vehicle.lidar.get_surrounding_vehicles()) != 0:
+                detect_vehicle = True
 
-        # env.render("Test: {}".format(i))
-        if d:
-            break
-    assert detect_vehicle, "Lidar detection failed"
-    env.close()
+            # env.render("Test: {}".format(i))
+            if d:
+                break
+        assert detect_vehicle, "Lidar detection failed"
+    finally:
+        env.close()
 
 
 if __name__ == "__main__":
