86d99b28ed7fb33ac86da03719e02e2dcbd492e1
lqy0057 <785878978@qq.com>
2021-02-24 14:56:40 +0800

    fix pytest bug

--- pgdrive/tests/test_vehicle/test_lidar.py
+++ pgdrive/tests/test_vehicle/test_lidar.py
@@ -11,8 +11,8 @@ class TestEnv(PGDriveEnv):
 def test_lidar(render=False):
     setup_logger(debug=True)
     Lidar.enable_show = True
+    env = TestEnv({"use_render": render, "manual_control": render, "environment_num": 1, "traffic_density": 0.3})
     try:
-        env = TestEnv({"use_render": render, "manual_control": render, "environment_num": 1, "traffic_density": 0.3})
         env.reset()
         detect_vehicle = False
         for i in range(1, 100000):
