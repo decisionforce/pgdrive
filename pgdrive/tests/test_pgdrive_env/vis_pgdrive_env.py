e11b46c8f68fed933c1bac8f9a2d0054ea6c7641
lqy0057 <785878978@qq.com>
2021-02-25 15:33:49 +0800

    format

--- pgdrive/tests/test_pgdrive_env/vis_pgdrive_env.py
+++ pgdrive/tests/test_pgdrive_env/vis_pgdrive_env.py
@@ -47,9 +47,13 @@ if __name__ == "__main__":
     print("vehicle num", len(env.scene_manager.traffic_mgr.vehicles))
     for i in range(1, 100000):
         o, r, d, info = env.step([0, 1])
-        env.render(text={"vehicle_num": len(env.scene_manager.traffic_mgr.traffic_vehicles),
-                         "dist_to_left:":env.vehicle.dist_to_left,
-                         "dist_to_right:":env.vehicle.dist_to_right})
+        env.render(
+            text={
+                "vehicle_num": len(env.scene_manager.traffic_mgr.traffic_vehicles),
+                "dist_to_left:": env.vehicle.dist_to_left,
+                "dist_to_right:": env.vehicle.dist_to_right
+            }
+        )
         # if d:
         #     print("Reset")
         #     env.reset()
