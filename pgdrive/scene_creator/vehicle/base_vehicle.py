3e8666ef8c760604168129c4d8f0ce97f9ef63ba
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-03-25 21:48:49 +0800

    move collisioni group

--- pgdrive/scene_creator/vehicle/base_vehicle.py
+++ pgdrive/scene_creator/vehicle/base_vehicle.py
@@ -10,9 +10,8 @@ import numpy as np
 from panda3d.bullet import BulletVehicle, BulletBoxShape, ZUp, BulletGhostNode
 from panda3d.core import Vec3, TransformState, NodePath, LQuaternionf, BitMask32, PythonCallbackObject, TextNode
 from pgdrive.scene_creator.vehicle.base_vehicle_node import BaseVehilceNode
-from pgdrive.constants import RENDER_MODE_ONSCREEN, COLOR, COLLISION_INFO_COLOR, BodyName, CamMask
+from pgdrive.constants import RENDER_MODE_ONSCREEN, COLOR, COLLISION_INFO_COLOR, BodyName, CamMask, CollisionGroup
 from pgdrive.pg_config import PGConfig
-from pgdrive.pg_config.collision_group import CollisionGroup
 from pgdrive.pg_config.parameter_space import Parameter, VehicleParameterSpace
 from pgdrive.pg_config.pg_space import PGSpace
 from pgdrive.rl_utils.cost import pg_cost_scheme
