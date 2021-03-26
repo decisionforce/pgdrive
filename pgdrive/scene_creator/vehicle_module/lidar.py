68afa564482ac0b2f0156d2058c24f547f964860
lqy0057 <785878978@qq.com>
2021-03-26 16:58:09 +0800

    revert

--- pgdrive/scene_creator/vehicle_module/lidar.py
+++ pgdrive/scene_creator/vehicle_module/lidar.py
@@ -17,24 +17,17 @@ class Lidar:
     DEFAULT_HEIGHT = 0.2
 
     def __init__(self, parent_node_np: NodePath, num_lasers: int = 240, distance: float = 50, enable_show=False):
+        # lidar properties
         show = enable_show and (AssetLoader.loader is not None)
         self.Lidar_point_cloud_obs_dim = num_lasers
         self.num_lasers = num_lasers
-
-        # lidar properties
         self.perceive_distance = distance
-        self.radian_unit = 2 * np.pi / num_lasers
         self.height = self.DEFAULT_HEIGHT
 
-        # used to save detection results of each frame
-        self.dynamic_elements = []
-        self.dynamic_cloud_points = []
-        self.static_elements = []
-        self.static_points = []
-
-        # visable
+        self.radian_unit = 2 * np.pi / num_lasers
+        self.detection_results = []
         self.node_path = parent_node_np.attachNewNode("cloudPoints")
-        self.node_path.hide(CamMask.RgbCam | CamMask.Shadow | CamMask.Shadow | CamMask.DepthCam)
+        self.node_path.hide(CamMask.RgbCam | CamMask.Shadow | CamMask.MainCam | CamMask.Shadow | CamMask.DepthCam)
         self.cloud_points = [] if show else None
         logging.debug("Load Vehicle Module: {}".format(self.__class__.__name__))
         if show:
@@ -57,10 +50,10 @@ class Lidar:
         """
         # coordinates problem here! take care
         pg_start_position = panda_position(vehicle_position, self.height)
+        self.detection_results = []
 
         # lidar calculation use pg coordinates
-        mask = BitMask32.bit(PGTrafficVehicle.COLLISION_MASK) | BitMask32.bit(CollisionGroup.EgoVehicle) | \
-               BitMask32.bit(CollisionGroup.LaneLine)
+        mask = BitMask32.bit(PGTrafficVehicle.COLLISION_MASK) | BitMask32.bit(CollisionGroup.EgoVehicle)
         laser_heading = np.arange(0, self.num_lasers) * self.radian_unit + heading_theta
         point_x = self.perceive_distance * np.cos(laser_heading) + vehicle_position[0]
         point_y = self.perceive_distance * np.sin(laser_heading) + vehicle_position[1]
@@ -68,21 +61,13 @@ class Lidar:
         for laser_index in range(self.num_lasers):
             # # coordinates problem here! take care
             laser_end = panda_position((point_x[laser_index], point_y[laser_index]), self.height)
-            results = pg_physics_world.dynamic_world.rayTestAll(pg_start_position, laser_end, mask)
-
-            dynamic_point_pos = laser_end
-            static_point_pos = laser_end
-            for result  in results.getHits():
-                if result.getNode().getName() ==BodyName.Sidewalk:
-                    self.static_results.append(result)
-                    static_point_pos = result.getHitPos()
-                else:
-                    self.dynamic_results.append(result)
-                    dynamic_point_pos = result.getHitPos()
-
-
-
+            result = pg_physics_world.dynamic_world.rayTestClosest(pg_start_position, laser_end, mask)
+            self.detection_results.append(result)
             if self.cloud_points is not None:
+                if result.hasHit():
+                    curpos = result.getHitPos()
+                else:
+                    curpos = laser_end
                 # if 0<=laser_index < 10 or 230 <= laser_index <=239:
                 #     self.cloud_points[laser_index].setColor(1,0,0)
                 self.cloud_points[laser_index].setPos(curpos)
@@ -91,7 +76,7 @@ class Lidar:
 
     def get_surrounding_vehicles(self) -> Set[IDMVehicle]:
         vehicles = set()
-        for ret in self.dynamic_results:
+        for ret in self.detection_results:
             if ret.hasHit() and ret.getNode().hasPythonTag(BodyName.Traffic_vehicle):
                 vehicles.add(ret.getNode().getPythonTag(BodyName.Traffic_vehicle).kinematic_model)
         return vehicles
@@ -102,7 +87,7 @@ class Lidar:
     #     :return: a set of objects
     #     """
     #     objects = set()
-    #     for ret in self.dynamic_results:
+    #     for ret in self.detection_results:
     #         if ret.hasHit() and ret.getNode().getName() in [BodyName.Traffic_cone, BodyName.Traffic_triangle]:
     #             objects.add(ret.getNode().getPythonTag(BodyName.Traffic_vehicle).kinematic_model)
     #     return objects
@@ -131,27 +116,15 @@ class Lidar:
                 res += [0.0] * 4
         return res
 
-    def get_dynamic_cloud_points(self):
-        return self.dynamic_cloud_points
-
-    def get_static_cloud_points(self):
-        return self.static_points
+    def get_cloud_points(self):
+        return [point.getHitFraction() for point in self.detection_results]
 
     def destroy(self):
         if self.cloud_points:
             for vis_laser in self.cloud_points:
                 vis_laser.removeNode()
         self.node_path.removeNode()
-        self.dynamic_elements = None
-        self.dynamic_cloud_points = None
-        self.static_elements = None
-        self.static_points = None
-
-    def reset(self):
-        self.dynamic_elements = []
-        self.dynamic_cloud_points = []
-        self.static_elements = []
-        self.static_points = []
+        self.detection_results = None
 
     def __del__(self):
         logging.debug("Lidar is destroyed.")
