0f4369c5a73fd3fbccbb0149e51d9b011665144f
lqy0057 <785878978@qq.com>
2021-03-26 14:31:58 +0800

    format

--- pgdrive/scene_creator/vehicle/base_vehicle.py
+++ pgdrive/scene_creator/vehicle/base_vehicle.py
@@ -83,7 +83,7 @@ class BaseVehicle(DynamicElement):
     WIDTH = None
 
     def __init__(
-            self, pg_world: PGWorld, vehicle_config: dict = None, physics_config: dict = None, random_seed: int = 0
+        self, pg_world: PGWorld, vehicle_config: dict = None, physics_config: dict = None, random_seed: int = 0
     ):
         """
         This Vehicle Config is different from self.get_config(), and it is used to define which modules to use, and
@@ -286,8 +286,7 @@ class BaseVehicle(DynamicElement):
         default: 3rd gear, try to use ae^bx to fit it, dp: (90, 8), (130, 12)
         :return: None
         """
-        self.energy_consumption += 3.25*np.power(np.e,0.01*self.speed)
-
+        self.energy_consumption += 3.25 * np.power(np.e, 0.01 * self.speed)
 
     def reset(self, map: Map, pos: np.ndarray = None, heading: float = 0.0):
         """
@@ -450,8 +449,8 @@ class BaseVehicle(DynamicElement):
             return 0
         # cos = self.forward_direction.dot(lateral) / (np.linalg.norm(lateral) * np.linalg.norm(self.forward_direction))
         cos = (
-                (forward_direction[0] * lateral[0] + forward_direction[1] * lateral[1]) /
-                (lateral_norm * forward_direction_norm)
+            (forward_direction[0] * lateral[0] + forward_direction[1] * lateral[1]) /
+            (lateral_norm * forward_direction_norm)
         )
         # return cos
         # Normalize to 0, 1
@@ -704,7 +703,7 @@ class BaseVehicle(DynamicElement):
             ckpt_idx = routing.target_checkpoints_index
             for surrounding_v in surrounding_vs:
                 if surrounding_v.lane_index[:-1] == (routing.checkpoints[ckpt_idx[0]], routing.checkpoints[ckpt_idx[1]
-                ]):
+                                                                                                           ]):
                     if self.lane.local_coordinates(self.position)[0] - \
                             self.lane.local_coordinates(surrounding_v.position)[0] < 0:
                         self.front_vehicles.add(surrounding_v)
@@ -719,7 +718,7 @@ class BaseVehicle(DynamicElement):
 
     @classmethod
     def get_action_space_before_init(cls):
-        return gym.spaces.Box(-1.0, 1.0, shape=(2,), dtype=np.float32)
+        return gym.spaces.Box(-1.0, 1.0, shape=(2, ), dtype=np.float32)
 
     def remove_display_region(self):
         if self.render:
