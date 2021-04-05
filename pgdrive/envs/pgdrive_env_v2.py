3d4b7b819bc2c6071891ba7fa3b125c3cb7f9a52
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-04-05 23:03:14 +0800

    update

--- pgdrive/envs/pgdrive_env_v2.py
+++ pgdrive/envs/pgdrive_env_v2.py
@@ -1,14 +1,83 @@
 import logging
-import os.path as osp
 
+import gym
 import numpy as np
-
 from pgdrive.constants import DEFAULT_AGENT
 from pgdrive.envs.pgdrive_env import PGDriveEnv as PGDriveEnvV1
+from pgdrive.obs import ImageStateObservation, ObservationType, LidarStateObservation
 from pgdrive.scene_manager.traffic_manager import TrafficMode
 from pgdrive.utils import PGConfig, clip
 
-pregenerated_map_file = osp.join(osp.dirname(osp.dirname(osp.abspath(__file__))), "assets", "maps", "PGDrive-maps.json")
+
+class LidarStateObservationV2(LidarStateObservation):
+    def __init__(self, vehicle_config):
+        super(LidarStateObservationV2, self).__init__(vehicle_config)
+
+    @property
+    def observation_space(self):
+        shape = [21, ]
+        if self.config["lidar"]["num_lasers"] > 0 and self.config["lidar"]["distance"] > 0:
+            # Number of lidar rays and distance should be positive!
+            shape[0] += self.config["lidar"]["num_lasers"] + self.config["lidar"]["num_others"] * 4
+        return gym.spaces.Box(-0.0, 1.0, shape=tuple(shape), dtype=np.float32)
+
+    def observe(self, vehicle):
+        state = self.state_observe(vehicle)
+        other_v_info = []
+        if vehicle.lidar is not None:
+            if self.config["lidar"]["num_others"] > 0:
+                other_v_info += vehicle.lidar.get_surrounding_vehicles_info(vehicle,
+                                                                            self.config["lidar"]["num_others"])
+            other_v_info += self._add_noise_to_cloud_points(vehicle.lidar.get_cloud_points())
+        return np.concatenate((state, np.asarray(other_v_info)))
+
+    def _add_noise_to_cloud_points(self, points):
+        return super(LidarStateObservationV2, self)._add_noise_to_cloud_points(points)
+
+    def state_observe(self, vehicle):
+        navi_info = vehicle.routing_localization.get_navi_info()
+        navi_info = [navi_info[0], navi_info[1], navi_info[5], navi_info[6]]  # Only keep the checkpoints information!
+        ego_state = self.vehicle_state(vehicle)
+        return np.asarray(ego_state + navi_info, dtype=np.float32)
+
+    def vehicle_state(self, vehicle):
+        """
+        Wrap vehicle states to list
+        """
+        # update out of road
+        info = []
+        if hasattr(vehicle, "side_detector") and vehicle.side_detector is not None:
+            info += vehicle.side_detector.get_cloud_points()
+        else:
+            raise ValueError()
+
+        # current_reference_lane = vehicle.routing_localization.current_ref_lanes[-1]
+        info += [
+            # vehicle.heading_diff(current_reference_lane),
+            # Note: speed can be negative denoting free fall. This happen when emergency brake.
+            clip((vehicle.speed + 1) / (vehicle.max_speed + 1), 0.0, 1.0),
+            clip((vehicle.steering / vehicle.max_steering + 1) / 2, 0.0, 1.0),
+            clip((vehicle.last_current_action[0][0] + 1) / 2, 0.0, 1.0),
+            clip((vehicle.last_current_action[0][1] + 1) / 2, 0.0, 1.0)
+        ]
+        heading_dir_last = vehicle.last_heading_dir
+        heading_dir_now = vehicle.heading
+        cos_beta = heading_dir_now.dot(heading_dir_last
+                                       ) / (np.linalg.norm(heading_dir_now) * np.linalg.norm(heading_dir_last))
+        beta_diff = np.arccos(clip(cos_beta, 0.0, 1.0))
+        # print(beta)
+        yaw_rate = beta_diff / 0.1
+        # print(yaw_rate)
+        info.append(clip(yaw_rate, 0.0, 1.0))
+
+        if vehicle.lane_line_detector is not None:
+            info += vehicle.lane_line_detector.get_cloud_points()
+        # else:
+        #     _, lateral = vehicle.lane.local_coordinates(vehicle.position)
+        #     info.append(
+        #         clip((lateral * 2 / vehicle.routing_localization.get_current_lane_width() + 1.0) / 2.0, 0.0, 1.0)
+        #     )
+        return info
 
 
 class PGDriveEnvV2(PGDriveEnvV1):
@@ -43,7 +112,9 @@ class PGDriveEnvV2(PGDriveEnvV1):
 
                 # See: https://github.com/decisionforce/pgdrive/issues/297
                 vehicle_config=dict(
-                    lidar=dict(num_lasers=120, distance=50, num_others=0, gaussian_noise=0.0, dropout_prob=0.0)
+                    lidar=dict(num_lasers=120, distance=50, num_others=0, gaussian_noise=0.0, dropout_prob=0.0),
+                    side_detector=dict(num_lasers=6, distance=50),  # laser num, distance
+                    lane_line_detector=dict(num_lasers=6, distance=20),  # laser num, distance
                 ),
 
                 # Disable map loading!
@@ -69,7 +140,8 @@ class PGDriveEnvV2(PGDriveEnvV1):
             done = True
             logging.info("Episode ended! Reason: crash. ")
             done_info["crash_vehicle"] = True
-        elif vehicle.out_of_route or not vehicle.on_lane or vehicle.crash_sidewalk:
+        # elif vehicle.out_of_route or not vehicle.on_lane or vehicle.crash_sidewalk:
+        elif vehicle.on_yellow_continuous_line or (not vehicle.on_lane) or vehicle.crash_sidewalk:
             done = True
             logging.info("Episode ended! Reason: out_of_road.")
             done_info["out_of_road"] = True
@@ -142,6 +214,13 @@ class PGDriveEnvV2(PGDriveEnvV1):
             ret[v_id] = self.observations[v_id].observe(v)
         return ret[DEFAULT_AGENT] if self.num_agents == 1 else ret
 
+    def get_single_observation(self, vehicle_config: "PGConfig") -> "ObservationType":
+        if self.config["use_image"]:
+            o = ImageStateObservation(vehicle_config)
+        else:
+            o = LidarStateObservationV2(vehicle_config)
+        return o
+
 
 if __name__ == '__main__':
 
@@ -151,6 +230,7 @@ if __name__ == '__main__':
         assert env.observation_space.contains(obs)
         assert np.isscalar(reward)
         assert isinstance(info, dict)
+
 
     env = PGDriveEnvV2()
     try:
