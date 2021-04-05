a527e990e712b46829c7a927bcbc2c5da0c4183a
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-04-05 16:46:12 +0800

    format

--- pgdrive/obs/state_obs.py
+++ pgdrive/obs/state_obs.py
@@ -13,7 +13,6 @@ class StateObservation(ObservationType):
     """
     Use vehicle state info, navigation info and lidar point clouds info as input
     """
-
     def __init__(self, config):
         super(StateObservation, self).__init__(config)
 
@@ -21,7 +20,7 @@ class StateObservation(ObservationType):
     def observation_space(self):
         # Navi info + Other states
         shape = self.Ego_state_obs_dim + RoutingLocalizationModule.Navi_obs_dim + self.get_side_detector_dim()
-        return gym.spaces.Box(-0.0, 1.0, shape=(shape,), dtype=np.float32)
+        return gym.spaces.Box(-0.0, 1.0, shape=(shape, ), dtype=np.float32)
 
     def observe(self, vehicle):
         """
@@ -153,9 +152,10 @@ class LidarStateObservation(ObservationType):
     def _add_noise_to_cloud_points(self, points):
         if self.config["lidar"]["gaussian_noise"] > 0.0:
             points = np.asarray(points)
-            points = np.clip(points + np.random.normal(
-                loc=0.0, scale=self.config["lidar"]["gaussian_noise"], size=points.shape
-            ), 0.0, 1.0)
+            points = np.clip(
+                points + np.random.normal(loc=0.0, scale=self.config["lidar"]["gaussian_noise"], size=points.shape),
+                0.0, 1.0
+            )
 
         if self.config["lidar"]["dropout_prob"] > 0.0:
             assert self.config["lidar"]["dropout_prob"] <= 1.0
