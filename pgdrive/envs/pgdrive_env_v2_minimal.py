fad1aa0c4c5013c8e9aed07a55942337bba115eb
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-04-06 21:45:51 +0800

    minor update

--- pgdrive/envs/pgdrive_env_v2_minimal.py
+++ pgdrive/envs/pgdrive_env_v2_minimal.py
@@ -153,7 +153,7 @@ class PGDriveEnvV2Minimal(PGDriveEnvV2):
         config = super(PGDriveEnvV2Minimal, cls).default_config()
         config.update({"num_others": 4, "use_extra_state": True})
         config["vehicle_config"]["lidar"]["distance"] = 200
-        config["vehicle_config"]["side_detector"]["num_lasers"] = 2
+        config["vehicle_config"]["side_detector"]["num_lasers"] = 0
         config["vehicle_config"]["side_detector"]["distance"] = 200
         config["vehicle_config"]["lane_line_detector"]["num_lasers"] = 0
         return config
