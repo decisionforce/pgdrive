a29c17f765b306432db62897d801479ac45de60d
lqy0057 <785878978@qq.com>
2021-04-11 17:05:17 +0800

    format

--- pgdrive/world/collision_callback.py
+++ pgdrive/world/collision_callback.py
@@ -22,13 +22,13 @@ def pg_collision_callback(contact):
             if another_node_name in [BodyName.Traffic_vehicle, BodyName.Ego_vehicle]:
                 nodes[i].getPythonTag(BodyName.Ego_vehicle).crash_vehicle = True
             # crash objects
-            elif another_node_name == BodyName.Traffic_cone and not another_nodes[
-                i].getPythonTag(BodyName.Traffic_cone).crashed:
+            elif another_node_name == BodyName.Traffic_cone and not another_nodes[i].getPythonTag(BodyName.Traffic_cone
+                                                                                                  ).crashed:
                 nodes[i].getPythonTag(BodyName.Ego_vehicle).crash_object = True
                 if another_nodes[i].getPythonTag(BodyName.Traffic_cone).COST_ONCE:
                     another_nodes[i].getPythonTag(BodyName.Traffic_cone).crashed = True
-            elif another_node_name == BodyName.Traffic_triangle and not another_nodes[
-                i].getPythonTag(BodyName.Traffic_triangle).crashed:
+            elif another_node_name == BodyName.Traffic_triangle and not another_nodes[i].getPythonTag(
+                    BodyName.Traffic_triangle).crashed:
                 nodes[i].getPythonTag(BodyName.Ego_vehicle).crash_object = True
                 if another_nodes[i].getPythonTag(BodyName.Traffic_cone).COST_ONCE:
                     another_nodes[i].getPythonTag(BodyName.Traffic_triangle).crashed = True
