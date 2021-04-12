771aa1e6c0d3794ab80ce56ce6b16de7a310cffc
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-04-12 10:51:23 +0800

    update map for roundabout

--- pgdrive/scene_creator/blocks/block.py
+++ pgdrive/scene_creator/blocks/block.py
@@ -27,6 +27,7 @@ class BlockSocket:
     Positive_road is right road, and Negative road is left road on which cars drive in reverse direction
     BlockSocket is a part of block used to connect other blocks
     """
+
     def __init__(self, positive_road: Road, negative_road: Road = None):
         self.positive_road = positive_road
         self.negative_road = negative_road if negative_road else None
@@ -55,6 +56,7 @@ class Block(Element, BlockDefault):
     When single-direction block created, road_2 in block socket is useless.
     But it's helpful when a town is created.
     """
+
     def __init__(self, block_index: int, pre_block_socket: BlockSocket, global_network: RoadNetwork, random_seed):
         super(Block, self).__init__(random_seed)
         # block information
@@ -110,11 +112,14 @@ class Block(Element, BlockDefault):
             self.side_normal = self.loader.loadTexture(AssetLoader.file_path("textures", "sidewalk", "normal.png"))
             self.sidewalk = self.loader.loadModel(AssetLoader.file_path("models", "box.bam"))
 
-    def construct_block(self, root_render_np: NodePath, pg_physics_world: PGPhysicsWorld) -> bool:
+    def construct_block(self, root_render_np: NodePath, pg_physics_world: PGPhysicsWorld,
+                        extra_config: Dict = None) -> bool:
         """
         Randomly Construct a block, if overlap return False
         """
         self.set_config(self.PARAMETER_SPACE.sample())
+        if extra_config:
+            self.set_config(extra_config)
         success = self._sample_topology()
         self._create_in_world()
         self.attach_to_pg_world(root_render_np, pg_physics_world)
@@ -391,14 +396,14 @@ class Block(Element, BlockDefault):
         body_np.setQuat(LQuaternionf(numpy.cos(theta / 2), 0, 0, numpy.sin(theta / 2)))
 
     def _add_lane_line2bullet(
-        self,
-        lane_start,
-        lane_end,
-        middle,
-        parent_np: NodePath,
-        color: Vec4,
-        line_type: LineType,
-        straight_stripe=False
+            self,
+            lane_start,
+            lane_end,
+            middle,
+            parent_np: NodePath,
+            color: Vec4,
+            line_type: LineType,
+            straight_stripe=False
     ):
         length = norm(lane_end[0] - lane_start[0], lane_end[1] - lane_start[1])
         if length <= 0:
