ea9a7abca3735119107767d09f6a849a3d4ab627
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-04-12 17:11:12 +0800

    minor

--- pgdrive/tests/vis_funtionality/vis_vehicle_num.py
+++ pgdrive/tests/vis_funtionality/vis_vehicle_num.py
@@ -1,20 +1,16 @@
 from pgdrive.envs.pgdrive_env_v2 import PGDriveEnvV2
-from pgdrive.scene_creator.map import Map, MapGenerateMethod
 
 if __name__ == "__main__":
     env = PGDriveEnvV2(
         {
-            "environment_num": 10000,
-            # "traffic_density": 0.1,
-            # "traffic_mode": 0,  # 0 for Reborn mode.
-            # "map_config": {
-            #     Map.GENERATE_TYPE: MapGenerateMethod.BIG_BLOCK_NUM,
-            #     Map.GENERATE_CONFIG: 7,
-            # }
+            "environment_num": 100,
+            "start_seed": 5000,
+            "traffic_density": 0.08,
         }
     )
     env.reset()
-    for i in range(1, 100000):
+    count = []
+    for i in range(1, 101):
         o, r, d, info = env.step([0, 1])
         env.reset()
         print(
@@ -22,4 +18,6 @@ if __name__ == "__main__":
                 env.current_seed, env.scene_manager.traffic_mgr.get_vehicle_num()
             )
         )
+        count.append(env.scene_manager.traffic_mgr.get_vehicle_num())
+    print(min(count), sum(count) / len(count), max(count))
     env.close()
