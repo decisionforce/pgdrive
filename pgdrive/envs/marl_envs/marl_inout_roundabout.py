e2d64b691d67201bcc13da38f6cc021194e94b17
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-04-13 15:20:19 +0800

    format

--- pgdrive/envs/marl_envs/marl_inout_roundabout.py
+++ pgdrive/envs/marl_envs/marl_inout_roundabout.py
@@ -43,6 +43,7 @@ class TargetVehicleManager:
     vehicle name: unique name for each vehicle instance, random string.
     agent name: agent name that exists in the environment, like agent0, agent1, ....
     """
+
     def __init__(self, ):
         self.agent_to_vehicle = {}
         self.vehicle_to_agent = {}
@@ -265,7 +266,7 @@ class MultiAgentRoundaboutEnv(MultiAgentPGDrive):
                     lane_tuple = road.lane_index(lane_idx)  # like (>>>, 1C0_0_, 1) and so on.
                     target_vehicle_configs.append(
                         dict(
-                            identifier="|".join((str(s) for s in lane_tuple + (j, ))),
+                            identifier="|".join((str(s) for s in lane_tuple + (j,))),
                             config={
                                 "born_lane_index": lane_tuple,
                                 "born_longitude": long,
@@ -294,15 +295,8 @@ class MultiAgentRoundaboutEnv(MultiAgentPGDrive):
         return config
 
     def reset(self, *args, **kwargs):
-        # self._next_agent_id = self.num_agents
-        # self._do_not_reborn = False
-        self.target_vehicle_manager.set_allow_reborn(True)
-        # self.done_vehicles.update(self.pending_vehicles)
-        # self.pending_vehicles = dict()
-
         # Shuffle born places!
         self.config = self._update_agent_pos_configs(self.config)
-
         ret = super(MultiAgentRoundaboutEnv, self).reset(*args, **kwargs)
         assert len(self.vehicles) == self.num_agents
         self.for_each_vehicle(self._update_destination_for)
@@ -338,8 +332,8 @@ class MultiAgentRoundaboutEnv(MultiAgentPGDrive):
 
         # Fulfill __all__
         d["__all__"] = (
-            ((self.episode_steps >= self.config["horizon"]) and (all(d.values()))) or (len(self.vehicles) == 0)
-            or (self.episode_steps >= 5 * self.config["horizon"])
+                ((self.episode_steps >= self.config["horizon"]) and (all(d.values()))) or (len(self.vehicles) == 0)
+                or (self.episode_steps >= 5 * self.config["horizon"])
         )
         if d["__all__"]:
             for k in d.keys():
