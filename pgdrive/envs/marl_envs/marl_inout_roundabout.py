7e5dc7608ae63119679f176a30da8c4a33434310
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-04-14 01:21:40 +0800

    format

--- pgdrive/envs/marl_envs/marl_inout_roundabout.py
+++ pgdrive/envs/marl_envs/marl_inout_roundabout.py
@@ -44,7 +44,6 @@ class TargetVehicleManager:
     vehicle name: unique name for each vehicle instance, random string.
     agent name: agent name that exists in the environment, like agent0, agent1, ....
     """
-
     def __init__(self, ):
         self.agent_to_vehicle = {}
         self.vehicle_to_agent = {}
@@ -270,7 +269,7 @@ class MultiAgentRoundaboutEnv(MultiAgentPGDrive):
                     lane_tuple = road.lane_index(lane_idx)  # like (>>>, 1C0_0_, 1) and so on.
                     target_vehicle_configs.append(
                         dict(
-                            identifier="|".join((str(s) for s in lane_tuple + (j,))),
+                            identifier="|".join((str(s) for s in lane_tuple + (j, ))),
                             config={
                                 "born_lane_index": lane_tuple,
                                 "born_longitude": long,
@@ -337,11 +336,11 @@ class MultiAgentRoundaboutEnv(MultiAgentPGDrive):
         condition = set(kkk for kkk, rrr in r.items() if rrr == self.config["success_reward"]) == \
                     set(kkk for kkk, iii in i.items() if iii.get("arrive_dest")) and condition
         condition = (
-                            not self.config["crash_done"] or (
-                            set(kkk for kkk, rrr in r.items() if rrr == -self.config["crash_vehicle_penalty"])
-                            == set(kkk for kkk, iii in i.items() if iii.get("crash_vehicle"))
-                    )
-                    ) and condition
+            not self.config["crash_done"] or (
+                set(kkk for kkk, rrr in r.items() if rrr == -self.config["crash_vehicle_penalty"])
+                == set(kkk for kkk, iii in i.items() if iii.get("crash_vehicle"))
+            )
+        ) and condition
         if not condition:
             raise ValueError("Observation not aligned!")
 
@@ -359,8 +358,8 @@ class MultiAgentRoundaboutEnv(MultiAgentPGDrive):
 
         # Update __all__
         d["__all__"] = (
-                ((self.episode_steps >= self.config["horizon"]) and (all(d.values()))) or (len(self.vehicles) == 0)
-                or (self.episode_steps >= 5 * self.config["horizon"])
+            ((self.episode_steps >= self.config["horizon"]) and (all(d.values()))) or (len(self.vehicles) == 0)
+            or (self.episode_steps >= 5 * self.config["horizon"])
         )
         if d["__all__"]:
             for k in d.keys():
