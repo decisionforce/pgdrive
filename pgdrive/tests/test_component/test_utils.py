5ca9c06f684758d97b510e7892cfd2ec6102dbf6
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-04-17 13:24:55 +0800

    format

--- pgdrive/tests/test_component/test_utils.py
+++ pgdrive/tests/test_component/test_utils.py
@@ -5,7 +5,7 @@ from pgdrive.utils.cutils import import_cutils
 
 def _test_cutils(cutils):
     for _ in range(20):
-        pos = np.random.normal(1000, 1000, size=(2,))
+        pos = np.random.normal(1000, 1000, size=(2, ))
         n = cutils.cutils_norm(*pos.tolist())
         assert n == np.linalg.norm(pos)
 
@@ -40,7 +40,7 @@ def test_utils():
     assert np.dot(get_vertical_vector([0, 1])[1], [0, 1]) == 0
 
     for _ in range(20):
-        pos = np.random.normal(0, 1, size=(2,))
+        pos = np.random.normal(0, 1, size=(2, ))
         assert distance_greater(pos, (0, 0), 0.5) == (np.linalg.norm(pos) > 0.5)
 
 
