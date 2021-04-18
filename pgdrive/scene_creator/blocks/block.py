4a794b2bea132b8c359fd8adb41c4352d9a46c06
pengzhenghao <pengzh@ie.cuhk.edu.hk>
2021-04-18 19:01:18 +0800

    fix

--- pgdrive/scene_creator/blocks/block.py
+++ pgdrive/scene_creator/blocks/block.py
@@ -16,7 +16,7 @@ from pgdrive.scene_creator.road.road_network import RoadNetwork
 from pgdrive.utils.asset_loader import AssetLoader
 from pgdrive.utils.coordinates_shift import panda_position
 from pgdrive.utils.element import Element
-from pgdrive.utils.math_utils import norm
+from pgdrive.utils.math_utils import norm, PGVector
 from pgdrive.world.pg_physics_world import PGPhysicsWorld
 
 
@@ -464,7 +464,7 @@ class Block(Element, BlockDefault):
             else:
                 factor = (1 + self.SIDEWALK_WIDTH / radius) * (1 + self.SIDEWALK_LINE_DIST / radius)
         direction_v = lane_end - lane_start
-        vertical_v = (-direction_v[1], direction_v[0]) / norm(*direction_v)
+        vertical_v = PGVector((-direction_v[1], direction_v[0])) / norm(*direction_v)
         middle += vertical_v * (self.SIDEWALK_WIDTH / 2 + self.SIDEWALK_LINE_DIST)
         side_np.setPos(panda_position(middle, 0))
         theta = -numpy.arctan2(direction_v[1], direction_v[0])
