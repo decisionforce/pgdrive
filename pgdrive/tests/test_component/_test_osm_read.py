217b1dcd13ac8f7fbbfa2a1d5db42cd88dd5b25f
lqy0057 <785878978@qq.com>
2021-07-27 13:12:56 +0800

    add test map

--- pgdrive/tests/test_component/_test_osm_read.py
+++ pgdrive/tests/test_component/_test_osm_read.py
@@ -1,6 +1,9 @@
 from pyrosm import OSM, get_data
 import osmnx as ox
 
+# get real map in PBF fromat from https://extract.bbbike.org/
+# install pyrosm and all dependencies via pip install pyrosm osmnx networkx
+
 # Initialize the reader
 osm = OSM("cpii.osm.pbf")
 
@@ -8,6 +11,7 @@ osm = OSM("cpii.osm.pbf")
 nodes, edges = osm.get_network(nodes=True, network_type="driving")
 # Create NetworkX graph
 G = osm.to_graph(nodes, edges, graph_type="networkx")
-ox.plot_graph(G)
+
 ccc = edges.head(40)
 print(ccc.geometry)
+ox.plot_graph(G)
