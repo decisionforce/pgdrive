a108b2bb2e6a6cd5fae508ba310f6825c2362e89
lqy0057 <785878978@qq.com>
2021-08-04 14:19:33 +0800

    fix idm test

--- pgdrive/tests/test_policy/test_idm_policy.py
+++ pgdrive/tests/test_policy/test_idm_policy.py
@@ -54,7 +54,7 @@ def test_idm_policy_briefly():
 
 def test_idm_policy_is_moving(render=False, in_test=True):
     # config = {"traffic_mode": "hybrid", "map": "SS", "traffic_density": 1.0}
-    config = {"traffic_mode": "hybrid", "map": "SS"}
+    config = {"traffic_mode": "hybrid", "map": "SS", "traffic_density":0.2}
     if render:
         config.update({"use_render": True, "fast": True, "manual_control": True})
     env = PGDriveEnvV2(config)
