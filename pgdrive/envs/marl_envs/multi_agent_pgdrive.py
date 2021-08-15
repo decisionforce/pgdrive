9a7fd134c50d9e7e5e6b3455012e71dec04c41bf
PENG Zhenghao <pengzh@ie.cuhk.edu.hk>
2021-08-15 13:42:43 +0800

    format

--- pgdrive/envs/marl_envs/multi_agent_pgdrive.py
+++ pgdrive/envs/marl_envs/multi_agent_pgdrive.py
@@ -148,8 +148,8 @@ class MultiAgentPGDrive(PGDriveEnvV2):
 
         # Update __all__
         d["__all__"] = (
-                ((self.episode_steps >= self.config["horizon"]) and (all(d.values()))) or (len(self.vehicles) == 0)
-                or (self.episode_steps >= 5 * self.config["horizon"])
+            ((self.episode_steps >= self.config["horizon"]) and (all(d.values()))) or (len(self.vehicles) == 0)
+            or (self.episode_steps >= 5 * self.config["horizon"])
         )
         if d["__all__"]:
             for k in d.keys():
