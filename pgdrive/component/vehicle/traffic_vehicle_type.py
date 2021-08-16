2933fbb4e8040a679b85de0eaa3b0b131145a9c0
lqy0057 <785878978@qq.com>
2021-08-16 19:08:56 +0800

    align model and box

--- pgdrive/component/vehicle/traffic_vehicle_type.py
+++ pgdrive/component/vehicle/traffic_vehicle_type.py
@@ -7,17 +7,26 @@ class XLVehicle(TrafficVehicle):
     LENGTH = 5.8
     WIDTH = 2.3
     HEIGHT = 2.8
-    # path = [['new/truck/scene.gltf', (factor * 0.031, factor * 0.025, factor * 0.025), (0.35, 0, factor * 0), 0]]
-    path = [['new/truck/scene.gltf', (factor, factor, factor), (0, 0, 0), 0]]
+    TIRE_RADIUS = 0.37
+    REAR_WHEELBASE = 1.075
+    FRONT_WHEELBASE = 1.726
+    LATERAL_TIRE_TO_CENTER = 0.831
+    CHASSIS_TO_WHEEL_AXIS = 0.3
+    MASS = 1600
+    path = [['new/truck/scene.gltf', (factor, factor, factor), (0, 0.3, 0.04), 0]]
 
 
 class LVehicle(TrafficVehicle):
     LENGTH = 4.5
     WIDTH = 1.86
     HEIGHT = 1.85
+    TIRE_RADIUS = 0.39
+    REAR_WHEELBASE = 1.10751
+    FRONT_WHEELBASE = 1.391
+    LATERAL_TIRE_TO_CENTER = 0.75
+    MASS = 1300
     path = [
-        # ['new/lada/scene.gltf', (factor * 1.1, factor * 1.1, factor * 1.1), (1.1, -13.5, factor * -0.046), 223],
-        ['new/lada/scene.gltf', (factor, factor, factor), (0, 0, 0), 0],
+        ['new/lada/scene.gltf', (factor, factor, factor), (0, -0.25, 0.07), 0],
     ]
 
 
@@ -25,9 +34,14 @@ class MVehicle(TrafficVehicle):
     LENGTH = 4.4
     WIDTH = 1.85
     HEIGHT = 1.37
+    TIRE_RADIUS = 0.39
+    REAR_WHEELBASE = 1.203
+    FRONT_WHEELBASE = 1.285
+    LATERAL_TIRE_TO_CENTER = 0.803
+    MASS = 1200
+
     path = [
-        # ['new/130/scene.gltf', (factor * .0055, factor * .0046, factor * .0049), (0, 0, factor * 0.33), 90],
-        ['new/130/scene.gltf', (factor, factor, factor), (0, 0, 0), 0],
+        ['new/130/scene.gltf', (factor, factor, factor), (0, -0.05, 0.07), 0],
     ]
 
 
@@ -35,8 +49,14 @@ class SVehicle(TrafficVehicle):
     LENGTH = 4.25
     WIDTH = 1.7
     HEIGHT = 1.7
+    LATERAL_TIRE_TO_CENTER = 0.7
+    FRONT_WHEELBASE = 1.4126
+    REAR_WHEELBASE = 1.07
+    TIRE_RADIUS = 0.376
+    MASS = 800
+
     path = [
-        ['new/beetle/scene.gltf', (factor, factor, factor), (0, 0, 0), 0],
+        ['new/beetle/scene.gltf', (factor, factor, factor), (0, -0.2, 0.03), 0],
     ]
 
 
