a34ae73753a0cd565a464d2aeac2727389864221
lqy0057 <785878978@qq.com>
2021-08-22 18:34:22 +0800

    fix env

--- pgdrive/policy/env_input_policy.py
+++ pgdrive/policy/env_input_policy.py
@@ -1,4 +1,5 @@
 from pgdrive.policy.base_policy import BasePolicy
+from pgdrive.utils.math_utils import clip
 
 
 class EnvInputPolicy(BasePolicy):
@@ -7,17 +8,20 @@ class EnvInputPolicy(BasePolicy):
         super(EnvInputPolicy, self).__init__(control_object=None)
         self.discrete_action = self.engine.global_config["discrete_action"]
         self.steering_unit = 2.0 / (
-            self.engine.global_config["discrete_steering_dim"] - 1
+                self.engine.global_config["discrete_steering_dim"] - 1
         )  # for discrete actions space
         self.throttle_unit = 2.0 / (
-            self.engine.global_config["discrete_throttle_dim"] - 1
+                self.engine.global_config["discrete_throttle_dim"] - 1
         )  # for discrete actions space
 
     def act(self, agent_id):
+        # clip to -1, 1
+        action = [clip(self.engine.external_actions[agent_id][i], -1.0, 1.0) for i in
+                  range(len(self.engine.external_actions[agent_id]))]
         if not self.discrete_action:
-            return self.engine.external_actions[agent_id]
+            return action
         else:
-            return self.convert_to_continuous_action(self.engine.external_actions[agent_id])
+            return self.convert_to_continuous_action(action)
 
     def convert_to_continuous_action(self, action):
         steering = action[0] * self.steering_unit - 1.0
